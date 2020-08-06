352747481bdc8ddea6dd3613b78e95fbfdfab115
mullally <mullally@zivitech.com>
2020-08-06 14:50:34 +0300

    console log deleted

--- src/SearchMovie.js
+++ src/SearchMovie.js
@@ -22,7 +22,6 @@ class SearchMovie extends React.Component {
 
     handleSubmit(e) {
         e.preventDefault()
-        console.log(this.state.query)
         const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${this.state.query}&page=1&include_adult=false`;
         console.log(url)
         this.getData(url)
@@ -37,7 +36,6 @@ class SearchMovie extends React.Component {
 
     render() {
         const isResult = !this.state.result
-        console.log(this.state.result)
         return (
             <div className="searchMovie" >
                 <form className="form"
